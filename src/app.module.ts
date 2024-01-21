d4b6132713d5e35f924aa6c8fac5cc012ba231f3
Aus Abrakhim <a.abrakhim@proscom.ru>
2024-01-21 19:21:50 +0300

    git init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import {
+  KeycloakConnectModule,
+  AuthGuard,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
 
 @Module({
-  imports: [],
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8081',
+      realm: 'VIOLET_REALM',
+      clientId: 'violet-admin',
+      secret: 'secret',
+      // policyEnforcement: PolicyEnforcementMode.PERMISSIVE, // optional
+      //tokenValidation: TokenValidation.ONLINE, // optional
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
